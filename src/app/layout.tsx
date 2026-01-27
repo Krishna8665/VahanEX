77aad9da63c085c9585a0841f0603d01e4f90616
Krishna8665 <khatrikrissna11@gmail.com>
2026-01-27 15:46:32 +0545

    chnages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Navbar } from "@/components/layout/Navbar";
+import Header from "@/components/layout/Header";
 import Sidebar from "@/components/layout/Sidebar";
 
 const inter = Inter({
